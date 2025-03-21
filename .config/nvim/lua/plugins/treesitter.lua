05632888dc2d8be021bebceaece01dcd97be08e8
marsbsp <marsbsp.r@proton.me>
2025-03-21 18:59:22 +0200

    add ejs parser and minor changes

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -16,9 +16,19 @@ return {
 				"markdown",
 				"json",
 				"scss",
+                "ejs",
 			},
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
+        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        parser_config.ejs = {
+            install_info = {
+                url = "https://github.com/tree-sitter/tree-sitter-embedded-template",
+                files = { "src/parser.c" },
+                requires_generate_from_grammar = true,
+            },
+            filetype = "ejs",
+        }
 	end,
 }
